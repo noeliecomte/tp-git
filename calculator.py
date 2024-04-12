22768024e6ab30e583f9cdeeb2651de30ad6f5f2
noeliecomte <noelie.comte02@gmail.com>
2024-04-12 15:30:55 +0200

    add docstrings

--- calculator.py
+++ calculator.py
@@ -1,10 +1,29 @@
 def addition(a,b):
+    """
+    :type a: float
+    :type b: float
+    :rtype: float
+    :return: result of addition
+    """
     result=a+b
     return result
 
 def substraction(a,b):
+    """
+    :type a: float
+    :type b: float
+    :rtype: float
+    :return: result of substraction
+    """
     result=a-b
     return result
 
 def division(a,b):
-    return a/b
+    """
+    :type a: float
+    :type b: float
+    :rtype: float
+    :return: result of division
+    """
+    result = a/b
+    return result
