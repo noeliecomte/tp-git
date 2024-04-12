6e2671287ced2bf761c57c0902fd01e8d026ffab
noeliecomte <noelie.comte02@gmail.com>
2024-04-12 15:36:59 +0200

    add docstrings

--- test_calculator.py
+++ test_calculator.py
@@ -3,12 +3,21 @@ from calculator import substraction
 from calculator import division
 
 def test_addition():
+    """
+    :function: unit test of addiction function
+    """
     assert addition(1, 2) == 3
 
 
 def test_substraction():
+    """
+    :function: unit test of addiction function
+    """
     assert substraction(2, 1) == 1
 
 
 def test_division():
+    """
+    :function: unit test of addiction function
+    """
     assert division(6, 2) == 3
